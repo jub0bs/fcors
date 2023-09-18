dc67643dde90e39f6f2ee8bf1bdef735b3c595cc
jub0bs <jcretel-infosec+github@protonmail.com>
2023-09-18 12:22:03 +0200

    systematically benchmark middleware initialization

--- bench_test.go
+++ bench_test.go
@@ -10,26 +10,6 @@ import (
 	"github.com/jub0bs/fcors"
 )
 
-func BenchmarkMiddlewareInitialization(b *testing.B) {
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		fcors.AllowAccessWithCredentials(
-			fcors.FromOrigins(
-				"https://*.example0.com",
-				"https://*.example1.com",
-				"https://*.example2.com",
-				"https://*.example3.com",
-				"https://*.example4.com",
-				"https://*.example5.com",
-				"https://*.example6.com",
-				"https://*.example7.com",
-			),
-			fcors.WithRequestHeaders("Authorization"),
-			fcors.WithMethods(http.MethodPut),
-		)
-	}
-}
-
 type middleware struct {
 	name string
 	cors fcors.Middleware
@@ -42,120 +22,134 @@ type request struct {
 	headers http.Header
 }
 
-var requestHeadersAllowedByDefaultInRsCORS = fcors.WithRequestHeaders(
-	"Accept",
-	"Content-Type",
-	"X-Requested-With",
-)
-
-func mustAllowAccess(tb testing.TB, one fcors.OptionAnon, others ...fcors.OptionAnon) fcors.Middleware {
-	tb.Helper()
-	cors, err := fcors.AllowAccess(one, others...)
-	if err != nil {
-		tb.Fatal("invalid policy")
-	}
-	return cors
-}
-
-func mustAllowAccessWithCredentials(tb testing.TB, one fcors.Option, others ...fcors.Option) fcors.Middleware {
-	tb.Helper()
-	cors, err := fcors.AllowAccessWithCredentials(one, others...)
-	if err != nil {
-		tb.Fatal("invalid policy")
-	}
-	return cors
+func requestHeadersAllowedByDefaultInRsCORS() fcors.Option {
+	return fcors.WithRequestHeaders(
+		"Accept",
+		"Content-Type",
+		"X-Requested-With",
+	)
 }
 
 func identity[T any](t T) T { return t }
 
-func BenchmarkMiddlewareInvocation(b *testing.B) {
-	var middlewares = []middleware{
+func BenchmarkMiddleware(b *testing.B) {
+	var middlewares []middleware
+	mw := middleware{
+		name: "without CORS config",
+		cors: identity[http.Handler],
+	}
+	mw.reqs = []request{
 		{
-			name: "without CORS config",
-			cors: identity[http.Handler],
-			reqs: []request{
-				{
-					name:   "CORS preflight request from some origin",
-					method: http.MethodOptions,
-					headers: http.Header{
-						headerOrigin: {"https://example.com"},
-						headerACRM:   {http.MethodGet},
-						headerACRH:   {"authorization"},
-					},
-				}, {
-					name:   "actual CORS request from some origin",
-					method: http.MethodGet,
-					headers: http.Header{
-						headerOrigin: {"https://example.com"},
-					},
-				},
+			name:   "CORS preflight request from some origin",
+			method: http.MethodOptions,
+			headers: http.Header{
+				headerOrigin: {"https://example.com"},
+				headerACRM:   {http.MethodGet},
+				headerACRH:   {"authorization"},
 			},
 		}, {
-			name: "allow arbitrary origin",
-			cors: mustAllowAccess(
-				b,
+			name:   "actual CORS request from some origin",
+			method: http.MethodGet,
+			headers: http.Header{
+				headerOrigin: {"https://example.com"},
+			},
+		},
+	}
+	middlewares = append(middlewares, mw)
+
+	var (
+		cors fcors.Middleware
+		err  error
+		f    func(b *testing.B)
+	)
+
+	mw = middleware{name: "allow arbitrary origin"}
+	f = func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			cors, err = fcors.AllowAccess(
 				fcors.FromAnyOrigin(),
-				requestHeadersAllowedByDefaultInRsCORS,
-			),
-			reqs: []request{
-				{
-					name:   "CORS preflight request from some origin",
-					method: http.MethodOptions,
-					headers: http.Header{
-						headerOrigin: {"https://example.com"},
-						headerACRM:   {http.MethodGet},
-						headerACRH:   {"authorization"},
-					},
-				}, {
-					name:   "actual CORS request from some origin",
-					method: http.MethodGet,
-					headers: http.Header{
-						headerOrigin: {"https://example.com"},
-					},
-				},
+				requestHeadersAllowedByDefaultInRsCORS(),
+			)
+			if err != nil {
+				b.Fatal(err)
+			}
+		}
+	}
+	b.Run(fmt.Sprintf("%s init", mw.name), f)
+	mw.cors = cors
+	mw.reqs = []request{
+		{
+			name:   "CORS preflight request from some origin",
+			method: http.MethodOptions,
+			headers: http.Header{
+				headerOrigin: {"https://example.com"},
+				headerACRM:   {http.MethodGet},
+				headerACRH:   {"authorization"},
 			},
 		}, {
-			name: "allow one origin",
-			cors: mustAllowAccess(
-				b,
+			name:   "actual CORS request from some origin",
+			method: http.MethodGet,
+			headers: http.Header{
+				headerOrigin: {"https://example.com"},
+			},
+		},
+	}
+	middlewares = append(middlewares, mw)
+
+	mw = middleware{name: "allow one origin"}
+	f = func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			cors, err = fcors.AllowAccess(
 				fcors.FromOrigins("https://example.com"),
-				requestHeadersAllowedByDefaultInRsCORS,
-			),
-			reqs: []request{
-				{
-					name:   "CORS preflight request from allowed origin",
-					method: http.MethodOptions,
-					headers: http.Header{
-						headerOrigin: {"https://example.com"},
-						headerACRM:   {http.MethodGet},
-						headerACRH:   {"authorization"},
-					},
-				}, {
-					name:   "actual CORS request from allowed origin",
-					method: http.MethodGet,
-					headers: http.Header{
-						headerOrigin: {"https://example.com"},
-					},
-				}, {
-					name:   "CORS preflight request from disallowed origin",
-					method: http.MethodOptions,
-					headers: http.Header{
-						headerOrigin: {"https://attacker.com"},
-						headerACRM:   {http.MethodGet},
-						headerACRH:   {"authorization"},
-					},
-				}, {
-					name:   "actual CORS request from disallowed origin",
-					method: http.MethodGet,
-					headers: http.Header{
-						headerOrigin: {"https://attacker.com"},
-					},
-				},
+				requestHeadersAllowedByDefaultInRsCORS(),
+			)
+			if err != nil {
+				b.Fatal(err)
+			}
+		}
+	}
+	b.Run(fmt.Sprintf("%s init", mw.name), f)
+	mw.cors = cors
+	mw.reqs = []request{
+		{
+			name:   "CORS preflight request from allowed origin",
+			method: http.MethodOptions,
+			headers: http.Header{
+				headerOrigin: {"https://example.com"},
+				headerACRM:   {http.MethodGet},
+				headerACRH:   {"authorization"},
 			},
 		}, {
-			name: "allow multiple origins",
-			cors: mustAllowAccess(
-				b,
+			name:   "actual CORS request from allowed origin",
+			method: http.MethodGet,
+			headers: http.Header{
+				headerOrigin: {"https://example.com"},
+			},
+		}, {
+			name:   "CORS preflight request from disallowed origin",
+			method: http.MethodOptions,
+			headers: http.Header{
+				headerOrigin: {"https://attacker.com"},
+				headerACRM:   {http.MethodGet},
+				headerACRH:   {"authorization"},
+			},
+		}, {
+			name:   "actual CORS request from disallowed origin",
+			method: http.MethodGet,
+			headers: http.Header{
+				headerOrigin: {"https://attacker.com"},
+			},
+		},
+	}
+	middlewares = append(middlewares, mw)
+
+	mw = middleware{name: "allow multiple origins"}
+	f = func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			cors, err = fcors.AllowAccess(
 				fcors.FromOrigins(
 					"https://*.example.com",
 					"https://*.google.com",
@@ -182,177 +176,224 @@ func BenchmarkMiddlewareInvocation(b *testing.B) {
 					"https://*.reddit.fr",
 					"https://*.quora.fr",
 				),
-				requestHeadersAllowedByDefaultInRsCORS,
-			),
-			reqs: []request{
-				{
-					name:   "CORS preflight request from allowed origin",
-					method: http.MethodOptions,
-					headers: http.Header{
-						headerOrigin: {"https://foo.quora.fr"},
-						headerACRM:   {http.MethodGet},
-						headerACRH:   {"authorization"},
-					},
-				}, {
-					name:   "actual CORS request from allowed origin",
-					method: http.MethodGet,
-					headers: http.Header{
-						headerOrigin: {"https://foo.quora.fr"},
-					},
-				}, {
-					name:   "CORS preflight request from disallowed origin",
-					method: http.MethodOptions,
-					headers: http.Header{
-						headerOrigin: {"https://attacker.com"},
-						headerACRM:   {http.MethodGet},
-						headerACRH:   {"authorization"},
-					},
-				}, {
-					name:   "actual CORS request from disallowed origin",
-					method: http.MethodGet,
-					headers: http.Header{
-						headerOrigin: {"https://attacker.com"},
-					},
-				}, {
-					name:   "CORS preflight request from abnormally long origin",
-					method: http.MethodOptions,
-					headers: http.Header{
-						headerOrigin: {abnormallyLongOrigin},
-						headerACRM:   {http.MethodGet},
-						headerACRH:   {"authorization"},
-					},
-				}, {
-					name:   "actual CORS request from abnormally long origin",
-					method: http.MethodGet,
-					headers: http.Header{
-						headerOrigin: {abnormallyLongOrigin},
-					},
-				},
+				requestHeadersAllowedByDefaultInRsCORS(),
+			)
+			if err != nil {
+				b.Fatal(err)
+			}
+		}
+	}
+	b.Run(fmt.Sprintf("%s init", mw.name), f)
+	mw.cors = cors
+	mw.reqs = []request{
+		{
+			name:   "CORS preflight request from allowed origin",
+			method: http.MethodOptions,
+			headers: http.Header{
+				headerOrigin: {"https://foo.quora.fr"},
+				headerACRM:   {http.MethodGet},
+				headerACRH:   {"authorization"},
 			},
 		}, {
-			name: "allow one pathological origin",
-			cors: mustAllowAccess(
-				b,
-				fcors.FromOrigins(
-					"https://a"+strings.Repeat(".a", 126),
-				),
-			),
-			reqs: []request{
-				{
-					name:   "CORS preflight request from pathological disallowed origin",
-					method: http.MethodOptions,
-					headers: http.Header{
-						headerOrigin: {"https://b" + strings.Repeat(".a", 126)},
-						headerACRM:   {http.MethodGet},
-						headerACRH:   {"authorization"},
-					},
-				}, {
-					name:   "actual CORS request from pathological disallowed origin",
-					method: http.MethodGet,
-					headers: http.Header{
-						headerOrigin: {"https://b" + strings.Repeat(".a", 126)},
-					},
-				}, {
-					name:   "CORS preflight request from allowed origin",
-					method: http.MethodOptions,
-					headers: http.Header{
-						headerOrigin: {"https://a" + strings.Repeat(".a", 126)},
-						headerACRM:   {http.MethodGet},
-						headerACRH:   {"authorization"},
-					},
-				}, {
-					name:   "actual CORS request from allowed origin",
-					method: http.MethodGet,
-					headers: http.Header{
-						headerOrigin: {"https://a" + strings.Repeat(".a", 126)},
-					},
-				},
+			name:   "actual CORS request from allowed origin",
+			method: http.MethodGet,
+			headers: http.Header{
+				headerOrigin: {"https://foo.quora.fr"},
 			},
 		}, {
-			name: "allow two pathological origins",
-			cors: mustAllowAccess(
-				b,
-				fcors.FromOrigins(
-					"https://a"+strings.Repeat(".a", 126),
-					"https://b"+strings.Repeat(".a", 126),
-				),
-			),
-			reqs: []request{
-				{
-					name:   "CORS preflight request from pathological disallowed origin",
-					method: http.MethodOptions,
-					headers: http.Header{
-						headerOrigin: {"https://c" + strings.Repeat(".a", 126)},
-						headerACRM:   {http.MethodGet},
-						headerACRH:   {"authorization"},
-					},
-				}, {
-					name:   "actual CORS request from pathological disallowed origin",
-					method: http.MethodGet,
-					headers: http.Header{
-						headerOrigin: {"https://c" + strings.Repeat(".a", 126)},
-					},
-				}, {
-					name:   "CORS preflight request from allowed origin",
-					method: http.MethodOptions,
-					headers: http.Header{
-						headerOrigin: {"https://a" + strings.Repeat(".a", 126)},
-						headerACRM:   {http.MethodGet},
-						headerACRH:   {"authorization"},
-					},
-				}, {
-					name:   "actual CORS request from allowed origin",
-					method: http.MethodGet,
-					headers: http.Header{
-						headerOrigin: {"https://a" + strings.Repeat(".a", 126)},
-					},
-				},
+			name:   "CORS preflight request from disallowed origin",
+			method: http.MethodOptions,
+			headers: http.Header{
+				headerOrigin: {"https://attacker.com"},
+				headerACRM:   {http.MethodGet},
+				headerACRH:   {"authorization"},
 			},
 		}, {
-			name: "allow one origin with credentials with any request headers",
-			cors: mustAllowAccessWithCredentials(
-				b,
-				fcors.FromOrigins("https://example.com"),
-				fcors.WithAnyRequestHeaders(),
-			),
-			reqs: []request{
-				{
-					name:   "CORS preflight request from allowed origin with adversarial ACRH",
-					method: http.MethodOptions,
-					headers: http.Header{
-						headerOrigin: {"https://example.com"},
-						headerACRM:   {http.MethodGet},
-						headerACRH:   {strings.Join(bigSliceOfJunk(10000), ", ")},
-					},
-				},
+			name:   "actual CORS request from disallowed origin",
+			method: http.MethodGet,
+			headers: http.Header{
+				headerOrigin: {"https://attacker.com"},
 			},
 		}, {
-			name: "allow one origin with credentials and expose some response headers",
-			cors: mustAllowAccessWithCredentials(
-				b,
-				fcors.FromOrigins("https://example.com"),
-				requestHeadersAllowedByDefaultInRsCORS,
-				fcors.ExposeResponseHeaders("foo", "bar", "baz"),
-			),
-			reqs: []request{
-				{
-					name:   "CORS preflight request from allowed origin",
-					method: http.MethodOptions,
-					headers: http.Header{
-						headerOrigin: {"https://example.com"},
-						headerACRM:   {http.MethodGet},
-						headerACRH:   {strings.Join(bigSliceOfJunk(10000), ", ")},
-					},
-				}, {
-					name:   "actual CORS request from allowed origin",
-					method: http.MethodGet,
-					headers: http.Header{
-						headerOrigin: {"https://example.com"},
-					},
-				},
+			name:   "CORS preflight request from abnormally long origin",
+			method: http.MethodOptions,
+			headers: http.Header{
+				headerOrigin: {abnormallyLongOrigin},
+				headerACRM:   {http.MethodGet},
+				headerACRH:   {"authorization"},
+			},
+		}, {
+			name:   "actual CORS request from abnormally long origin",
+			method: http.MethodGet,
+			headers: http.Header{
+				headerOrigin: {abnormallyLongOrigin},
 			},
 		},
 	}
+	middlewares = append(middlewares, mw)
+
+	mw = middleware{name: "allow one pathological origin"}
+	f = func(b *testing.B) {
+		origin := "https://a" + strings.Repeat(".a", 126)
+		b.ReportAllocs()
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			cors, err = fcors.AllowAccess(
+				fcors.FromOrigins(origin),
+			)
+			if err != nil {
+				b.Fatal(err)
+			}
+		}
+	}
+	b.Run(fmt.Sprintf("%s init", mw.name), f)
+	mw.cors = cors
+	mw.reqs = []request{
+		{
+			name:   "CORS preflight request from pathological disallowed origin",
+			method: http.MethodOptions,
+			headers: http.Header{
+				headerOrigin: {"https://b" + strings.Repeat(".a", 126)},
+				headerACRM:   {http.MethodGet},
+				headerACRH:   {"authorization"},
+			},
+		}, {
+			name:   "actual CORS request from pathological disallowed origin",
+			method: http.MethodGet,
+			headers: http.Header{
+				headerOrigin: {"https://b" + strings.Repeat(".a", 126)},
+			},
+		}, {
+			name:   "CORS preflight request from allowed origin",
+			method: http.MethodOptions,
+			headers: http.Header{
+				headerOrigin: {"https://a" + strings.Repeat(".a", 126)},
+				headerACRM:   {http.MethodGet},
+				headerACRH:   {"authorization"},
+			},
+		}, {
+			name:   "actual CORS request from allowed origin",
+			method: http.MethodGet,
+			headers: http.Header{
+				headerOrigin: {"https://a" + strings.Repeat(".a", 126)},
+			},
+		},
+	}
+	middlewares = append(middlewares, mw)
+
+	mw = middleware{name: "allow two pathological origins"}
+	f = func(b *testing.B) {
+		originA := "https://a" + strings.Repeat(".a", 126)
+		originB := "https://b" + strings.Repeat(".a", 126)
+		b.ReportAllocs()
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			cors, err = fcors.AllowAccess(
+				fcors.FromOrigins(originA, originB),
+			)
+			if err != nil {
+				b.Fatal(err)
+			}
+		}
+	}
+	b.Run(fmt.Sprintf("%s init", mw.name), f)
+	mw.cors = cors
+	mw.reqs = []request{
+		{
+			name:   "CORS preflight request from pathological disallowed origin",
+			method: http.MethodOptions,
+			headers: http.Header{
+				headerOrigin: {"https://c" + strings.Repeat(".a", 126)},
+				headerACRM:   {http.MethodGet},
+				headerACRH:   {"authorization"},
+			},
+		}, {
+			name:   "actual CORS request from pathological disallowed origin",
+			method: http.MethodGet,
+			headers: http.Header{
+				headerOrigin: {"https://c" + strings.Repeat(".a", 126)},
+			},
+		}, {
+			name:   "CORS preflight request from allowed origin",
+			method: http.MethodOptions,
+			headers: http.Header{
+				headerOrigin: {"https://a" + strings.Repeat(".a", 126)},
+				headerACRM:   {http.MethodGet},
+				headerACRH:   {"authorization"},
+			},
+		}, {
+			name:   "actual CORS request from allowed origin",
+			method: http.MethodGet,
+			headers: http.Header{
+				headerOrigin: {"https://a" + strings.Repeat(".a", 126)},
+			},
+		},
+	}
+	middlewares = append(middlewares, mw)
+
+	mw = middleware{name: "allow one origin with credentials with any request headers"}
+	f = func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			cors, err = fcors.AllowAccessWithCredentials(
+				fcors.FromOrigins("https://example.com"),
+				fcors.WithAnyRequestHeaders(),
+			)
+			if err != nil {
+				b.Fatal(err)
+			}
+		}
+	}
+	b.Run(fmt.Sprintf("%s init", mw.name), f)
+	mw.cors = cors
+	mw.reqs = []request{
+		{
+			name:   "CORS preflight request from allowed origin with adversarial ACRH",
+			method: http.MethodOptions,
+			headers: http.Header{
+				headerOrigin: {"https://example.com"},
+				headerACRM:   {http.MethodGet},
+				headerACRH:   {strings.Join(bigSliceOfJunk(10000), ", ")},
+			},
+		},
+	}
+	middlewares = append(middlewares, mw)
+
+	mw = middleware{name: "allow one origin with credentials and expose some response headers"}
+	f = func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			cors, err = fcors.AllowAccessWithCredentials(
+				fcors.FromOrigins("https://example.com"),
+				requestHeadersAllowedByDefaultInRsCORS(),
+				fcors.ExposeResponseHeaders("foo", "bar", "baz"),
+			)
+			if err != nil {
+				b.Fatal(err)
+			}
+		}
+	}
+	b.Run(fmt.Sprintf("%s init", mw.name), f)
+	mw.cors = cors
+	mw.reqs = []request{
+		{
+			name:   "CORS preflight request from allowed origin",
+			method: http.MethodOptions,
+			headers: http.Header{
+				headerOrigin: {"https://example.com"},
+				headerACRM:   {http.MethodGet},
+				headerACRH:   {strings.Join(bigSliceOfJunk(10000), ", ")},
+			},
+		}, {
+			name:   "actual CORS request from allowed origin",
+			method: http.MethodGet,
+			headers: http.Header{
+				headerOrigin: {"https://example.com"},
+			},
+		},
+	}
+	middlewares = append(middlewares, mw)
+
 	for _, mw := range middlewares {
 		handler := mw.cors(dummyHandler)
 		for _, req := range mw.reqs {
@@ -378,11 +419,13 @@ func BenchmarkMiddlewareInvocation(b *testing.B) {
 // As result, we incur a heap allocation when we add a second Vary header;
 // see the slow path of the fastAdd function.
 func BenchmarkMiddlewareInvocationVary(b *testing.B) {
-	cors := mustAllowAccess(
-		b,
+	cors, err := fcors.AllowAccess(
 		fcors.FromOrigins("https://*.example.com"),
-		requestHeadersAllowedByDefaultInRsCORS,
+		requestHeadersAllowedByDefaultInRsCORS(),
 	)
+	if err != nil {
+		b.Fatal(err)
+	}
 	handler := varyMiddleware(cors(dummyHandler))
 	reqs := []request{
 		{
